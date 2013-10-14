7ae3691ad12ed6e1ee87f388a80009fac68957cd
D.Maruta <rmswimkktt@gmail.com>
2013-10-14 13:11:55 +0900

    P.196 第5章まで完了

--- src/Section3Main.java
+++ src/Section3Main.java
@@ -5,7 +5,7 @@ public class Section3Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		RequestQueueImp requestQueueImp = new RequestQueue();
+		RequestQueueImp requestQueueImp = new RequestQueue(3000);
 		new ClientThread(requestQueueImp, "Alice", 1L).start();
 		new ServerThread(requestQueueImp, "Bobby", 2L).start();
 
