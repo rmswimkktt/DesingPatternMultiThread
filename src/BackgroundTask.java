fd5300c70b42edf563a5d64ea1c3f151382f80e4
D.Maruta <rmswimkktt@gmail.com>
2013-10-15 20:01:54 +0900

    マルチスレッドのJUnitTest

---
+++ src/BackgroundTask.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.Executors;
+
+
+public class BackgroundTask {
+	private final Runnable task;
+	
+	public BackgroundTask(Runnable task){
+		this.task = task;
+	}
+	
+	public void invoke(){
+		Executors.newSingleThreadExecutor().execute(task);
+	}
+}
