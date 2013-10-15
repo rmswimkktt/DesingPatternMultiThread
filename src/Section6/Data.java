ea6b83ff07a0ab5ea77aade4c5c4ce3e3d17591b
D.Maruta <rmswimkktt@gmail.com>
2013-10-15 20:52:39 +0900

    P.217 ReentrantReadWriteLockでDataインタフェースを実装

--- src/Section6/Data.java
+++ src/Section6/Data.java
@@ -1,52 +1,9 @@
 package Section6;
 
-import java.util.concurrent.locks.ReadWriteLock;
 
 
-public class Data {
-	private final char[] buffer;
-	private final ReadWriterLock lock = new ReadWriterLock();
-	public Data(int size){
-		this.buffer = new char[size];
-		for(int i = 0; i < buffer.length; i++){
-			buffer[i] = '*';
-		}
-	}
-	public char[] read() throws InterruptedException{
-		lock.readLock();
-		try{
-			return doRead();
-		}finally{
-			lock.readUnlock();
-		}
-	}
-	public void write(char c) throws InterruptedException{
-		lock.writeLock();
-		try{
-			doWrite(c);
-		}finally{
-			lock.writeUnlock();
-		}
-	}
-	private char[] doRead(){
-		char[] newbuf = new char[buffer.length];
-		for(int i = 0; i < buffer.length; i++){
-			newbuf[i] = buffer[i];
-		}
-		slowly();
-		return newbuf;
-	}
-	private void doWrite(char c){
-		for(int i = 0; i < buffer.length; i++){
-			buffer[i] = c;
-			slowly();
-		}
-	}
-	private void slowly(){
-		try{
-			Thread.sleep(50);
-		}catch(InterruptedException e){
-		}
-	}
+interface Data {
+	public char[] read() throws InterruptedException;
+	public void write(char c) throws InterruptedException;
 }
 
--- src/Section6/Main.java
+++ src/Section6/Main.java
@@ -3,7 +3,8 @@ package Section6;
 
 public class Main {
 	public static void main(String[] args){
-		Data data = new Data(10);
+//		Data data = new NormalData(10);
+		Data data = new LocksData(10);
 		new ReaderThread(data).start();
 		new ReaderThread(data).start();
 		new ReaderThread(data).start();
