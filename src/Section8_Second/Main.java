09e5663e50393ee751a58682f22d3cc3a5a99f21
D.Maruta <rmswimkktt@gmail.com>
2013-10-16 21:11:59 +0900

    P.286 補講2Executorsクラスで置き換え

---
+++ src/Section8_Second/Main.java
@@ -0,0 +1,20 @@
+package Section8_Second;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+	public static void main(String[] args){
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		try{
+			new ClientThread("Alice", executorService).start();
+			new ClientThread("Bobby", executorService).start();
+			new ClientThread("Chris", executorService).start();
+			
+			Thread.sleep(5000);
+		}catch(InterruptedException e){
+		}finally{
+			executorService.shutdown();
+		}
+	}
+}
