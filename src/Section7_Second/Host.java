cbc20a3f340a100d6ae0fb14fded1969c78be913
D.Maruta <rmswimkktt@gmail.com>
2013-10-16 10:24:22 +0900

    P.243 ThreadFactorを使用

---
+++ src/Section7_Second/Host.java
@@ -0,0 +1,26 @@
+package Section7_Second;
+
+import java.util.concurrent.ThreadFactory;
+
+import Section7.Helper;
+
+public class Host {
+	private final Helper helper = new Helper();
+	private final ThreadFactory threadFactory;
+	
+	public Host(ThreadFactory threadFactory){
+		this.threadFactory = threadFactory;
+	}
+	
+	public void request(final int count, final char c){
+		System.out.println("     request(" + count + ", " + c + ") BEGIN");
+		threadFactory.newThread(
+			new Runnable() {
+				
+				public void run() {
+					helper.handle(count, c);
+				}
+			}
+		).start();
+	}
+}
