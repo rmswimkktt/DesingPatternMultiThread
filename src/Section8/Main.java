c579c9e8baa17292e975394acf51fb7c4ea708fc
D.Maruta <rmswimkktt@gmail.com>
2013-10-16 21:46:34 +0900

    P.290 問題8-2

--- src/Section8/Main.java
+++ src/Section8/Main.java
@@ -2,8 +2,7 @@ package Section8;
 
 public class Main {
 	public static void main(String[] args){
-		Channel channel = new Channel(5);
-		channel.startWorkers();
+		Channel channel = new Channel();
 		new ClientThread("Alice", channel).start();
 		new ClientThread("Bobby", channel).start();
 		new ClientThread("Chris", channel).start();
