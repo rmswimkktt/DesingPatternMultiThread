1c9b31c3bf77e28545c900f208db49292a718fe4
D.Maruta <rmswimkktt@gmail.com>
2013-10-29 22:15:53 +0900

    問題の途中

---
+++ src/Question9_3/content/AyncContentImpl.java
@@ -0,0 +1,13 @@
+package Question9_3.content;
+
+public class AyncContentImpl implements Content {
+	private byte[] contentbytes;
+	public AyncContentImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public byte[] getBytes() {
+		return contentbytes;
+	}
+
+}
