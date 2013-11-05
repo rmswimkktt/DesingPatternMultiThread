2eb103e7dc67b9fc2a8f1c538081e33346bc5037
D.Maruta <rmswimkktt@gmail.com>
2013-11-05 22:14:38 +0900

    P.347 スレッドの同期

---
+++ src/Section10_Third/Main.java
@@ -0,0 +1,36 @@
+package Section10_Third;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+	private static final int THREADS = 3;
+	
+	public static void main(String[] args){
+		System.out.println("BEGIN");
+		ExecutorService service = Executors.newFixedThreadPool(THREADS);
+		Runnable barrierAction = new Runnable() {
+			
+			public void run() {
+				System.out.println("Barrier Action!");
+			}
+		};
+		CyclicBarrier phaseBarrier = new CyclicBarrier(THREADS, barrierAction);
+		CountDownLatch doneLatch = new CountDownLatch(THREADS);
+		
+		try{
+			for(int t = 0; t < THREADS; t++){
+				service.execute(new MyTask(phaseBarrier, doneLatch, t));
+			}
+			
+			System.out.println("AWAIT");
+			doneLatch.await();
+		}catch(InterruptedException e){
+		}finally{
+			service.shutdown();
+			System.out.println("END");
+		}
+	}
+}
---
+++ src/Section10_Third/MyTask.java
@@ -0,0 +1,52 @@
+package Section10_Third;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class MyTask implements Runnable {
+
+	private static final int PHASE = 5;
+	private final CyclicBarrier phaseBarrier;
+	private final CountDownLatch doneLatch;
+	private final int context;
+	private static final Random random = new Random(2);
+	
+	
+	
+	public MyTask(CyclicBarrier phaseBarrier, CountDownLatch doneLatch,
+			int context) {
+		super();
+		this.phaseBarrier = phaseBarrier;
+		this.doneLatch = doneLatch;
+		this.context = context;
+	}
+
+	public void run() {
+		try{
+			for(int phase = 0; phase < PHASE; phase++){
+				doPhase(phase);
+				phaseBarrier.await();
+			}
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}catch(BrokenBarrierException e){
+			e.printStackTrace();
+		}finally{
+			doneLatch.countDown();
+		}
+	}
+	
+	protected void doPhase(int phase){
+		String name = Thread.currentThread().getName();
+		System.out.println(name + ":MyTask:BEGIN:context = " + context + ", phase = " + phase);
+		try{
+			Thread.sleep(random.nextInt(3000));
+		}catch(InterruptedException e){
+		}finally{
+			System.out.println(name + ":MyTask:END:context = " + context + ", phase = " + phase);
+		}
+	}
+
+}
